07839e3d0e6134cf1fe2c908d01d2569fbbe48dd
BaebyBriel <brownerniebusty@gmail.com>
2024-08-23 10:11:06 +0000

    Initial commit for professor review app

--- components/App.tsx
+++ components/App.tsx
@@ -1,17 +1,14 @@
-// src/App.tsx
 import React from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import FeaturedProfessors from './components/FeaturedProfessors';
 
-const App: React.FC = () => {
-  return (
-    <div>
-      <Header />
-      <Hero />
-      <FeaturedProfessors />
-    </div>
-  );
-};
+const App: React.FC = () => (
+  <div className="app-container">
+    <Header />
+    <Hero />
+    <FeaturedProfessors />
+  </div>
+);
 
 export default App;
